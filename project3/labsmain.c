e6682003afbcf9481dd458cc0aab846a39289f53
Ilieshka <ilialubanov@gmail.com>
2024-05-15 22:54:40 +0300

    Update labsmain.c

--- project3/labsmain.c
+++ project3/labsmain.c
@@ -1,47 +1,50 @@
-
 #include <stdio.h>
 #include "labs.h"
+
 int main() {
-    
     int variant;
-do {
-    printf("Vvedite nomer lab: \n");
-    scanf_s("%d", &variant);
-    if (variant == 1) {
-        labs1();
-    }
-    else if (variant == 2){
-    
-        int labs2();
-    }
-    else if (variant == 3)
-    {
-        labs3();
-    }
-    else if (variant == 4) {
+    int correctVariant = 0; // Флаг для проверки правильности ввода
 
-        labs4();
-    }
-    else if (variant == 5) {
+    do {
+        printf("Vvedite nomer lab: \n");
+        scanf_s("%d", &variant);
 
-        labs5();
-    }
-    else if (variant == 6) {
+        if (variant == 1) {
+            labs1();
+            correctVariant = 1; // Устанавливаем флаг, если вариант правильный
+        }
+        else if (variant == 2) {
+            labs2();
+            correctVariant = 1;
+        }
+        else if (variant == 3) {
+            labs3();
+            correctVariant = 1;
+        }
+        else if (variant == 4) {
+            labs4();
+            correctVariant = 1;
+        }
+        else if (variant == 5) {
+            labs5();
+            correctVariant = 1;
+        }
+        else if (variant == 6) {
+            labs6();
+            correctVariant = 1;
+        }
+        else if (variant == 7) {
+            labs7();
+            correctVariant = 1;
+        }
+        else if (variant == 8) {
+            labs8();
+            correctVariant = 1;
+        }
+        else {
+            printf("Varianta net \n");
+        }
+    } while (!correctVariant); // Повторяем, пока флаг не установлен
 
-        labs6();
-    }
-    else if (variant == 7) {
-
-        labs7();
-    }
-    else if (variant == 8) {
-
-        labs8();
-    }
-    else
-    {
-        printf("Varianta net! \n");
-    }
-    } while (!variant);
     return 0;
 }
