99206474390fb7213f223acfa8ee3bd21bd05462
Ilieshka <ilialubanov@gmail.com>
2024-05-15 20:35:16 +0300

    Add files via upload

---
+++ project3/labsmain.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include "labs.h"
+int main() {
+    
+    int variant;
+
+    printf("Vvedite nomer lab: \n");
+    scanf_s("%d", &variant);
+    if (variant == 1) {
+        labs1();
+    }
+    else if (variant == 2){
+    
+        int labs2();
+    }
+    else if (variant == 3)
+    {
+        labs3();
+    }
+    else if (variant == 4) {
+
+        labs4();
+    }
+    else if (variant == 5) {
+
+        labs5();
+    }
+    else if (variant == 6) {
+
+        labs6();
+    }
+    else if (variant == 7) {
+
+        labs7();
+    }
+    else if (variant == 8) {
+
+        labs8();
+    }
+    else
+    {
+        printf("Varianta net ");
+    }
+    return 0;
+}
